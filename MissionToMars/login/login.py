5574fe4a41f6d7d97317b9afb93291c891a453c8
akbchris <450913976@qq.com>
2020-05-12 05:01:12 +1000

    finish login

---
+++ MissionToMars/login/login.py
@@ -0,0 +1,26 @@
+import sqlite3
+import os
+
+def clear():
+    os.system('cls')
+
+def checkLogin(userNames, psw):
+    re = 'Fail, please check'
+    for i in result:
+        if userNames == i[1] and psw == i[2]:
+            os.system('clear')
+            re = 'Success'
+    return re
+
+
+conn = sqlite3.connect('../marsdb.db')
+c = conn.cursor()
+c.execute('SELECT * FROM user')
+result = c.fetchall()
+
+print('Welcome to Mission to Mars System!!')
+username = input('enter you user name:')
+pwd = input('enter your password:')
+clear()
+print(checkLogin(username, pwd))
+conn.close()
