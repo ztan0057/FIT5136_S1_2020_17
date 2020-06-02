b7961b2c4d8168538f0d07aabe36a5e80236a00e
mjin0007 <63432104+mjin0007@users.noreply.github.com>
2020-06-03 01:53:28 +1000

    Create Model for shuttle

---
+++ MissionToMars/spaceShuttle/model/shuttle.py
@@ -0,0 +1,10 @@
+class Shuttle:
+    def __init__(self):
+        self._shuttle_id = None
+        self._name = None
+        self._manufacture_year = None
+        self._fuel_capacity = None
+        self._passenger_capacity = None
+        self._cargo_capacity = None
+        self._travel_speed = None
+        self._origin = None
