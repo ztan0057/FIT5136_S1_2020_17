b050d6a39a39e952eaf3a80586867a2d0c696759
mjin0007 <63432104+mjin0007@users.noreply.github.com>
2020-06-03 02:12:52 +1000

    create data loader to extract shuttle data from CSV

--- MissionToMars/spaceShuttle/spaceshuttleinf.py
+++ MissionToMars/spaceShuttle/spaceshuttleinf.py
@@ -1,1 +1,35 @@
+from datetime import datetime
+from model.shuttle import Shuttle
+import csv
+
+
+class ShuttleDataLoader:
+    def __init__(self):
+        self._shuttle_dict = {}  # dict of shuttle data indexed by IDs
+
+    def load_from_csv(self, path):
+        # Clear existing data
+        self._shuttle_dict = {}
+
+        with open(path, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                shuttle = Shuttle()
+
+                shuttle.shuttle_id = int(row['shuttle_id'])
+                shuttle.name = row['name']
+                shuttle.manufacture_year = datetime.strptime(row['manufacture_year'], '%m/%d/%Y')
+                shuttle.fuel_capacity = float(row['fuel_capacity'])
+                shuttle.passenger_capacity = int(row['passenger_capacity'])
+                shuttle.cargo_capacity = float(row['cargo_capacity'])
+                shuttle.travel_speed = float(row['travel_speed'])
+                shuttle.origin = row['origin']
+
+                self._shuttle_dict[shuttle.shuttle_id] = shuttle
+
+    def get_shuttle_list(self):
+        return self._shuttle_dict.values()
+
+    def get_shuttle_by_id(self, shuttle_id):
+        return self._shuttle_dict[shuttle_id]
 
